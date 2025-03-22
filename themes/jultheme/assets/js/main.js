f7eb16b858c9e9a685768b5f15ce5b015129851c
juljeryt <juljeryt@gmail.com>
2025-03-22 16:04:35 +0000

    make every img lazy load

--- themes/jultheme/assets/js/main.js
+++ themes/jultheme/assets/js/main.js
@@ -1,1 +1,6 @@
 console.log("Hello world!");
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll("img").forEach(img => {
+        img.setAttribute("loading", "lazy");
+    });
+});
