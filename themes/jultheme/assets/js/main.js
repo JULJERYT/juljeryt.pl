8805cc1a09356b4215341b82539c540c4cf7b335
juljeryt <juljeryt@gmail.com>
2025-03-23 11:38:34 +0000

    show ascii text in console and remove old debug console.log(s)

--- themes/jultheme/assets/js/main.js
+++ themes/jultheme/assets/js/main.js
@@ -1,4 +1,3 @@
-console.log("Hello world!");
 document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll("img").forEach(img => {
         img.setAttribute("loading", "lazy");
@@ -6,6 +5,12 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/assets/frameworks/sw.js').then(() => {
-        console.log("service worker registered");
+        // console.log("service worker registered");
     });
 }
+
+console.log(`%c
+    _         _   _                     _              _ \r\n   (_) _   _ | | (_)  ___  _ __  _   _ | |_     _ __  | |\r\n   | || | | || | | | \/ _ \\| \'__|| | | || __|   | \'_ \\ | |\r\n   | || |_| || | | ||  __\/| |   | |_| || |_  _ | |_) || |\r\n  _\/ | \\__,_||_|_\/ | \\___||_|    \\__, | \\__|(_)| .__\/ |_|\r\n |__\/          |__\/              |___\/         |_|       
+                     coded by jul                     
+                 https://juljeryt.pl/                 
+`, "background: linear-gradient(90deg, red, orange, yellow); color: transparent; -webkit-background-clip: text; font-weight: bold; font-family:monospace; font-size: 14.5px; ")
