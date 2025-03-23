e21d68b9f4cd278b1d8150f19ad17a6b7139d783
juljeryt <juljeryt@gmail.com>
2025-03-23 11:38:59 +0000

    build

---
+++ public/assets/js/main.daebae8a3aaba0b08925691a72ee959c3dee73aa6293a0099e9861200809d2ae.js
@@ -0,0 +1,10 @@
+(()=>{document.addEventListener("DOMContentLoaded",function(){document.querySelectorAll("img").forEach(e=>{e.setAttribute("loading","lazy")})}),"serviceWorker"in navigator&&navigator.serviceWorker.register("/assets/frameworks/sw.js").then(()=>{}),console.log(`%c
+    _         _   _                     _              _ \r
+   (_) _   _ | | (_)  ___  _ __  _   _ | |_     _ __  | |\r
+   | || | | || | | | / _ \\| '__|| | | || __|   | '_ \\ | |\r
+   | || |_| || | | ||  __/| |   | |_| || |_  _ | |_) || |\r
+  _/ | \\__,_||_|_/ | \\___||_|    \\__, | \\__|(_)| .__/ |_|\r
+ |__/          |__/              |___/         |_|       
+                     coded by jul                     
+                 https://juljeryt.pl/                 
+`,"background: linear-gradient(90deg, red, orange, yellow); color: transparent; -webkit-background-clip: text; font-weight: bold; font-family:monospace; font-size: 14.5px; ")})()
--- public/assets/js/main.eeeb1afe9fc2e2a46dd747ac53940546bbdef5545b5132c2d2cb1716ea20a162.js
+++
@@ -1,1 +0,0 @@
-(()=>{console.log("Hello world!"),document.addEventListener("DOMContentLoaded",function(){document.querySelectorAll("img").forEach(e=>{e.setAttribute("loading","lazy")})}),"serviceWorker"in navigator&&navigator.serviceWorker.register("/assets/frameworks/sw.js").then(()=>{console.log("service worker registered")})})()
