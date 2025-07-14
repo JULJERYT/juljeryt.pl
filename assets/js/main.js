8f4f2e425df87fd370e49976b152ad738cca51ae
juljeryt <kontakt@juljeryt.pl>
2025-07-14 20:15:22 +0200

    add animated title

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,3 +33,17 @@ async function updateStatus() {
 }
 
 updateStatus();
+
+// animated title
+function animateTitle() {
+    var index = 0;
+    var reverse = false;
+    var animatedTitle = "jul's website";
+    setInterval(function () {
+        index = (index + (reverse ? -1 : 1)) % (animatedTitle.length + 1);
+        if (index === 0 || index === animatedTitle.length) reverse = !reverse;
+        document.title = animatedTitle.slice(0, (index ? index : 1));
+    }, 300);
+}
+
+window.onload = animateTitle;
