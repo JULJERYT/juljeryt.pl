0f094642fba8be7aa117b9db72b03c2e1491a125
juljeryt <kontakt@juljeryt.pl>
2025-07-14 19:03:48 +0200

    use internal status api and move some things to second card

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,33 @@
-console.log('Hello World');
+async function updateStatus() {
+    try {
+        const response = await fetch('https://api.juljeryt.pl/status');
+        const data = await response.json();
+
+        if (!data.status) return;
+
+        let statusText;
+        switch (data.status) {
+            case 'online':
+                statusText = 'online 🟢';
+                break;
+            case 'dnd':
+                statusText = 'do not disturb ⛔';
+                break;
+            case 'idle':
+                statusText = 'idle 🌙';
+                break;
+            case 'offline':
+                statusText = 'offline 🔴';
+                break;
+            default:
+                return;
+        }
+
+        document.getElementById('status').textContent = statusText;
+        document.getElementById('status').removeAttribute('hidden');
+    } catch (error) {
+        console.error('failed to fetch status:', error);
+    }
+}
+
+updateStatus();
